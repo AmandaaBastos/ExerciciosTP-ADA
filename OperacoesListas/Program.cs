d44bc7a33e160deebb41486bca1f14f09d2cf10e
AmandaaBastos <amandabastoscorreia@gmail.com>
2024-01-29 10:01:24 -0300

    feat:API

---
+++ OperacoesListas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OperacoesListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<string>{
+              "Idiossincrasia",
+              "Ambivalente",
+              "Quimérica",
+              "Perpendicular",
+              "Efêmero",
+              "Pletora",
+              "Obnubilado",
+              "Xilografia",
+              "Quixote",
+              "Inefável"
+            };
+
+            foreach (var palavra in FiltrarTamanho(lista, 10))
+            {
+                Console.WriteLine(palavra);
+            }
+
+        }
+
+        static IEnumerable<string> FiltrarTamanho(List<string> lista, int tamanho)
+        {
+
+            foreach (var palavra in lista)
+            {
+                if (palavra.Length >= tamanho)
+                {
+                    yield return palavra;
+                }
+
+            }
+        }
+    }
+}
---
+++ OperacoesPilhas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OperacoesPilhas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
